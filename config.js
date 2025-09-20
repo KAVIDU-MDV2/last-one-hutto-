5fe043a1f7af87f660ee9b9406bbee3a63ab87fb
KAVIDU-MDV2 <kaviduasehnmdb.ot@gmail.com>
2025-09-20 21:55:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "IK~zkgH3ICZ#PUa6bgcUbnlWZNiWMUFgyRBj-5S0LMWExgPnW2kJGs4",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
